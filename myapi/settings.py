c5970264adc9ca9e3ac214432e1264d03120c043
Sumit Singh <sumit.singh4613@gmail.com>
2020-01-22 22:41:39 +0530

    minor changes

--- myapi/settings.py
+++ myapi/settings.py
@@ -74,7 +74,7 @@ WSGI_APPLICATION = "myapi.wsgi.application"
 # Rest Framework
 REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 5,
+    "PAGE_SIZE": 10,
 }
 
 
